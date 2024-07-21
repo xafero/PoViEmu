23278708ee0f61e43ce5eb24f2a2173a473e8083
xafero <xafero@users.noreply.github.com>
2024-07-21 15:28:11 +0200

    Adding dump model

---
+++ src/PoViEmu.Core/Dumps/DumpInfo.cs
@@ -0,0 +1,18 @@
+using PoViEmu.Common;
+
+namespace PoViEmu.Core.Dumps
+{
+    public sealed class DumpInfo
+    {
+        internal readonly DumpHeader _real;
+
+        public DumpInfo(DumpHeader header)
+            => _real = header;
+
+        public string Signature
+            => TextHelper.CleanUp(new string(_real.Signature));
+
+        public DumpModel Model
+            => TextHelper.ToEnum<DumpModel>(_real.Model, default);
+    }
+}
