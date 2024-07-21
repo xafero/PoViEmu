511b9e6c14c655f7596389f08a031b80454c7a10
xafero <xafero@users.noreply.github.com>
2024-07-21 15:42:02 +0200

    Adding reader wrappers

---
+++ src/PoViEmu.Core/AddInReader.cs
@@ -0,0 +1,51 @@
+using System;
+using System.IO;
+using PoViEmu.Common;
+using PoViEmu.Core.Addins;
+
+namespace PoViEmu.Core
+{
+    public static class AddInReader
+    {
+        public const int HeaderSize = 144;
+
+        public static AddInInfo Read(byte[] bytes)
+        {
+            var gotSize = bytes.Length;
+            if (gotSize != HeaderSize)
+                throw new InvalidOperationException($"Got {gotSize} bytes instead of {HeaderSize}!");
+            var header = Marshalling.Read<AddInHeader>(bytes);
+            var result = new AddInInfo(header);
+            CheckIfValid(result);
+            return result;
+        }
+
+        private static void CheckIfValid(AddInInfo result)
+        {
+            try
+            {
+                _ = result.HeaderVersion;
+                _ = result.LibraryVersion;
+                _ = result.Version;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw new InvalidOperationException("Could not parse the header!");
+            }
+        }
+
+        public static AddInInfo Read(Stream stream, int offset = 0)
+        {
+            var buffer = new byte[HeaderSize];
+            stream.Position = offset;
+            _ = stream.Read(buffer, 0, HeaderSize);
+            var header = Read(buffer);
+            return header;
+        }
+
+        public static AddInHeader GetInternal(this AddInInfo info)
+        {
+            return info._real;
+        }
+    }
+}
---
+++ src/PoViEmu.Core/DumpReader.cs
@@ -0,0 +1,50 @@
+using System;
+using System.IO;
+using PoViEmu.Common;
+using PoViEmu.Core.Dumps;
+
+namespace PoViEmu.Core
+{
+    public static class DumpReader
+    {
+        public const int HeaderSize = 144;
+
+        public static DumpInfo Read(byte[] bytes)
+        {
+            var gotSize = bytes.Length;
+            if (gotSize != HeaderSize)
+                throw new InvalidOperationException($"Got {gotSize} bytes instead of {HeaderSize}!");
+            var header = Marshalling.Read<DumpHeader>(bytes);
+            var result = new DumpInfo(header);
+            CheckIfValid(result);
+            return result;
+        }
+
+        private static void CheckIfValid(DumpInfo result)
+        {
+            try
+            {
+                _ = result.Signature;
+                _ = result.Model;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw new InvalidOperationException("Could not parse the header!");
+            }
+        }
+
+        public static DumpInfo Read(Stream stream, int offset = 0)
+        {
+            var buffer = new byte[HeaderSize];
+            stream.Position = offset;
+            _ = stream.Read(buffer, 0, HeaderSize);
+            var header = Read(buffer);
+            return header;
+        }
+
+        public static DumpHeader GetInternal(this DumpInfo info)
+        {
+            return info._real;
+        }
+    }
+}
