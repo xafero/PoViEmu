765013b78a86bff470ec473f5db0893bc4ea158d
xafero <xafero@users.noreply.github.com>
2024-08-04 20:55:59 +0200

    Adding sample

---
+++ tools/cpufuzz/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace CpuFuzzer
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var root = Environment.CurrentDirectory;
+            root = Path.GetFullPath(root);
+            Console.WriteLine($"Root = {root}");
+
+            Console.WriteLine("Done.");
+        }
+    }
+}
