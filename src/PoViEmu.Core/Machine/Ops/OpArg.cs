5eff316141bd6542d3bb244a948b74eda9e2b8a1
xafero <xafero@users.noreply.github.com>
2024-08-07 22:54:07 +0200

    Adding more fun

--- src/PoViEmu.Core/Machine/Ops/OpArg.cs
+++ src/PoViEmu.Core/Machine/Ops/OpArg.cs
@@ -1,9 +1,22 @@
 using PoViEmu.Core.Machine.Args;
+using PoViEmu.Expert;
 
 namespace PoViEmu.Core.Machine.Ops
 {
     public abstract class OpArg
     {
+        public static implicit operator OpArg(GenReg8 val)
+            => new RegisterArg((Register)val);
+
+        public static implicit operator OpArg(GenReg16 val)
+            => new RegisterArg((Register)val);
+        
+        public static implicit operator OpArg(IdxReg val)
+            => new RegisterArg((Register)val);
+        
+        public static implicit operator OpArg(SegReg val)
+            => new RegisterArg((Register)val);
+
         public static implicit operator OpArg(Register val) => new RegisterArg(val);
 
         public static implicit operator OpArg(int val) => new ConstantArg((byte)val);
