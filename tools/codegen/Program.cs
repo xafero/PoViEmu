5fc368f23b51ec5642abeb8cd622486b33a06b6b
xafero <xafero@users.noreply.github.com>
2024-08-09 21:45:45 +0200

    Adding extra project

---
+++ tools/codegen/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using PoViEmu.CpuFuzzer.App;
+
+namespace PoViEmu.CpuFuzzer
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var root = Environment.CurrentDirectory;
+            root = Path.GetFullPath(root);
+            Console.WriteLine($"Root = {root}");
+
+            var arg0 = args.FirstOrDefault();
+            switch (arg0)
+            {
+                default:
+                    CodeFuzz.Start();
+                    break;
+            }
+
+            Console.WriteLine("Done.");
+        }
+    }
+}
