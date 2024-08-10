bd5da7d4c5083ff7c25f7f0f198fd1813293d0c1
xafero <xafero@users.noreply.github.com>
2024-08-10 23:00:45 +0200

    Adding a lot of byte codes

---
+++ test/PoViEmu.Tests/InstrTestBase.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+using System.Linq;
+using PoViEmu.Core.Machine;
+using PoViEmu.Expert;
+
+namespace PoViEmu.Tests
+{
+    public abstract class InstrTestBase
+    {
+        protected void ShouldCheckThis(params int[] rawBytes)
+        {
+            var bytes = rawBytes.Select(b => (byte)b).ToArray();
+            var expected = NasmTool.DisassembleNasm(bytes);
+            var instr = new MemoryStream(bytes).Disassemble().ToArray();
+            var mis = instr.Where(i => i.Bytes.Length != i.Size).ToArray();
+
+            var actual = instr.ToText();
+            TestTool.Compare(expected, actual);
+
+            if (mis.Length >= 1)
+                throw new InvalidOperationException($"{string.Join("|",
+                    mis.Select(m => (m.Size, m.Bytes.Length)))} {mis.ToText()}");
+        }
+    }
+}
