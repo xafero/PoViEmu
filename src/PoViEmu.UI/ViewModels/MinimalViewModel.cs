4e6b6f6b401acae5487dd6332b608a13395c45b3
xafero <xafero@users.noreply.github.com>
2024-08-17 23:01:34 +0200

    Adding minimal view to save space

---
+++ src/PoViEmu.UI/ViewModels/MinimalViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using CommunityToolkit.Mvvm.ComponentModel;
+using PoViEmu.Common;
+using PoViEmu.UI.Models;
+using PoViEmu.X86Decoding;
+
+namespace PoViEmu.UI.ViewModels
+{
+    public partial class MinimalViewModel : ViewModelBase
+    {
+        [ObservableProperty] private string _title = string.Empty;
+    }
+}
