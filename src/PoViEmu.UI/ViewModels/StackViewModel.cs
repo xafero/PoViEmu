c6cf83ac7d65f4f2f537be0224b65f43123f6323
xafero <xafero@users.noreply.github.com>
2024-08-18 01:25:21 +0200

    Adding stack view

---
+++ src/PoViEmu.UI/ViewModels/StackViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using CommunityToolkit.Mvvm.ComponentModel;
+using PoViEmu.Common;
+using PoViEmu.UI.Models;
+using PoViEmu.X86Decoding;
+
+namespace PoViEmu.UI.ViewModels
+{
+    public partial class StackViewModel : ViewModelBase
+    {
+        [ObservableProperty] private ObservableCollection<StackLine> _stack = new();
+
+        public void Init()
+        {
+            Stack.Clear();
+
+            Stack.Add(new StackLine("SS", 0x0000, 0x002B));
+            Stack.Add(new StackLine("SS", 0x0002, 0xC400));
+            Stack.Add(new StackLine("SS", 0x0004, 0x16B4));
+            Stack.Add(new StackLine("SS", 0x0006, 0xC400));
+            Stack.Add(new StackLine("SS", 0x0008, 0x0BAF));
+        }
+    }
+}
