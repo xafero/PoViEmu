c9164f16ffda0d289186865a70a1f37d44776321
xafero <xafero@users.noreply.github.com>
2024-08-18 20:48:45 +0200

    Adding sim ctrl

--- src/PoViEmu.UI/ViewModels/SimCtrlViewModel.cs
+++ src/PoViEmu.UI/ViewModels/SimCtrlViewModel.cs
@@ -1,13 +1,32 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System;
+using System.Threading;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace PoViEmu.UI.ViewModels
 {
     public partial class SimCtrlViewModel : ViewModelBase
     {
-        [ObservableProperty] private string _title = string.Empty;
+        [ObservableProperty] private TimeSpan _cpuTime;
+        [ObservableProperty] private bool _shouldRun;
+
+        private Thread _thread;
 
         public void Init()
         {
+            CpuTime = TimeSpan.Zero;
+            ShouldRun = true;
+
+            _thread = new Thread(OnLoop);
+            _thread.Start();
+        }
+
+        private void OnLoop()
+        {
+            while (ShouldRun)
+            {
+                CpuTime = CpuTime.Add(TimeSpan.FromSeconds(1));
+                Thread.Sleep(1 * 500);
+            }
         }
     }
 }
