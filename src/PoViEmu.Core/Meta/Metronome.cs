0eb67e9b3832fe334a9149d3789fe9adec16cb13
xafero <xafero@users.noreply.github.com>
2024-08-18 14:49:55 +0200

    Adding sim ctrl

--- src/PoViEmu.Core/Meta/Metronome.cs
+++ src/PoViEmu.Core/Meta/Metronome.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace PoViEmu.Core.Meta
 {
     public sealed class Metronome
@@ -8,5 +10,41 @@ namespace PoViEmu.Core.Meta
          * "CLOCK": "25"
          * "LCDC_FREQ": "70"
          */
+
+        public void Step()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            // TODO Reset ?!
+            
+            throw new NotImplementedException();
+        }
+
+        public void Pause()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public static class X
+    {
+        public static void Do()
+        {
+            Metronome m = new Metronome();
+            m.Start();
+            m.Step();
+            m.Pause();
+            m.Stop();
+            
+            // Open / Close log file
+        }
     }
 }
---
+++ src/PoViEmu.UI/ViewModels/SimCtrlViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using CommunityToolkit.Mvvm.ComponentModel;
+using PoViEmu.Common;
+using PoViEmu.UI.Models;
+using PoViEmu.X86Decoding;
+
+namespace PoViEmu.UI.ViewModels
+{
+    public partial class SimCtrlViewModel : ViewModelBase
+    {
+        [ObservableProperty] private string _title = string.Empty;
+
+        public void Init()
+        {
+        }
+    }
+}
