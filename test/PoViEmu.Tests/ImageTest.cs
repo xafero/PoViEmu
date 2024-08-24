280ec2975faf520e70478cf6d6b355cc27fe91c8
xafero <xafero@users.noreply.github.com>
2024-08-24 21:16:36 +0200

    Removing superflouus tests

--- test/PoViEmu.Tests/ImageTest.cs
+++ test/PoViEmu.Tests/ImageTest.cs
@@ -11,10 +11,6 @@ namespace PoViEmu.Tests
         [InlineData("clown1.png")]
         [InlineData("clown2.bmp")]
         [InlineData("clown2.png")]
-        [InlineData("clown3.bmp")]
-        [InlineData("clown3.png")]
-        [InlineData("clown4.bmp")]
-        [InlineData("clown4.png")]
         public void ShouldCreateBin(string fileName)
         {
             var dir = Path.Combine("Resources", "Images");
@@ -24,8 +20,6 @@ namespace PoViEmu.Tests
         [Theory]
         [InlineData("clown1.bin")]
         [InlineData("clown2.bin")]
-        [InlineData("clown3.bin")]
-        [InlineData("clown4.bin")]
         public void ShouldCreateBmp(string fileName)
         {
             var dir = Path.Combine("Resources", "Images");
