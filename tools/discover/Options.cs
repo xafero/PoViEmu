7a0812859e8c4f7b44ec418355538bfa774dafc1
xafero <xafero@users.noreply.github.com>
2024-08-27 19:22:53 +0200

    Extending the tool

---
+++ tools/discover/Options.cs
@@ -0,0 +1,15 @@
+using CommandLine;
+
+// ReSharper disable ClassNeverInstantiated.Global
+
+namespace Discover
+{
+    public class Options
+    {
+        [Option('h', "hex", HelpText = "Find hex matches.")]
+        public bool HexMatch { get; set; }
+
+        [Option('i', "input", HelpText = "Set input directory.")]
+        public string InputDir { get; set; }
+    }
+}
--- tools/discover/Program.cs
+++ tools/discover/Program.cs
@@ -1,42 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+using CommandLine;
 
-namespace discover
+namespace Discover
 {
     internal static class Program
     {
         private static void Main(string[] args)
         {
-            var folder = args.FirstOrDefault()!;
-
-            const SearchOption o = SearchOption.AllDirectories;
-            var files = Directory.EnumerateFiles(folder, "*.*", o);
-
-            var dict = new Dictionary<string, string>();
-
-            foreach (var file in files)
+            var parser = Parser.Default;
+            parser.ParseArguments<Options>(args).WithParsed(o =>
             {
-                var bytes = File.ReadAllBytes(file);
-                var hex = Convert.ToHexString(bytes);
-                dict[file] = hex;
-            }
-
-            foreach (var hex in dict.Values.OrderBy(x => x.Length))
-            {
-                Console.WriteLine($" * Searching {hex.Length / 2} hex: {string.Join("", hex.Take(10))}");
-                var it = dict.Where(d => d.Value.Contains(hex)).ToArray();
-                if (it.Length < 2)
-                    continue;
-
-                foreach (var item in it)
+                if (o.HexMatch)
                 {
-                    var idx = item.Value.IndexOf(hex, StringComparison.InvariantCultureIgnoreCase);
-                    var end = idx + (hex.Length / 2);
-                    Console.WriteLine($"   # {item.Key}, at {idx} until {end}");
+                    HexMatch.Run(o);
                 }
-            }
+            });
         }
     }
 }
