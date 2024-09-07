75837dab160bfec80fb05029631000fcdde197a4
xafero <xafero@users.noreply.github.com>
2024-09-07 21:21:32 +0200

    Adding some ops

--- src/PoViEmu.Core/Decoding/StateExt.cs
+++ src/PoViEmu.Core/Decoding/StateExt.cs
@@ -1,4 +1,12 @@
+using System;
 using System.Linq;
+using System.Text;
+using Iced.Intel;
+using PoViEmu.Common;
+using R = Iced.Intel.Register;
+using OK = Iced.Intel.OpKind;
+
+#pragma warning disable CS8509
 
 namespace PoViEmu.Core.Decoding
 {
@@ -6,12 +14,103 @@ namespace PoViEmu.Core.Decoding
     {
         public const int StackSize = 2;
 
-        public static void PushStack(this MachineState state, ushort value)
+        public static void Push(this MachineState state, ushort value)
         {
             state.SP -= StackSize;
 
             var list = state.Stack.First();
             list.Value.Insert(0, value);
         }
+
+        public static void Pop(this MachineState state, out ushort value)
+        {
+            var list = state.Stack.First();
+            value = list.Value.First();
+            list.Value.RemoveAt(0);
+
+            state.SP += StackSize;
+        }
+
+        public static ushort GetValue(this MachineState state, Instruction instr, int opNr)
+        {
+            return opNr switch
+            {
+                1 => instr.Op1Kind switch
+                {
+                    OK.Immediate16 => instr.Immediate16,
+                    OK.Register => state.Get(instr.Op1Register),
+                },
+                _ => throw new InvalidOperationException($"{instr} / {opNr}")
+            };
+        }
+
+        public static ushort Get(this MachineState state, R register)
+        {
+            return register switch
+            {
+                R.AX => state.AX,
+                R.BX => state.BX,
+                R.CX => state.CX,
+                R.DX => state.DX,
+                R.SP => state.SP,
+                R.BP => state.BP,
+                R.SI => state.SI,
+                R.DI => state.DI,
+                R.ES => state.ES,
+                R.CS => state.CS,
+                R.SS => state.SS,
+                R.DS => state.DS,
+                _ => throw new InvalidOperationException($"{register}")
+            };
+        }
+        
+        public static void Set(this MachineState state, R register, ushort value)
+        {
+            switch (register)
+            {
+                case R.AX: state.AX = value; break;
+                case R.BX: state.BX = value; break;
+                case R.CX: state.CX = value; break;
+                case R.DX: state.DX = value; break;
+                case R.SP: state.SP = value; break;
+                case R.BP: state.BP = value; break;
+                case R.SI: state.SI = value; break;
+                case R.DI: state.DI = value; break;
+                case R.ES: state.ES = value; break; 
+                case R.CS: state.CS = value; break;
+                case R.SS: state.SS = value; break;
+                case R.DS: state.DS = value; break;
+                default: throw new InvalidOperationException($"{register}");
+            }
+        }
+
+        public static string? ToTxt(this R register)
+            => register == default ? null : register.ToString();
+
+        public static string GetOpDebug(this Instruction i)
+            => string.Join(" ; ", Enumerable.Range(0, i.OpCount)
+                .Select(n =>
+                {
+                    var kind = i.GetOpKind(n);
+                    var val = string.Empty;
+                    switch (kind)
+                    {
+                        case OK.Register:
+                            val = "R_" + i.GetOpRegister(n);
+                            break;
+                        case OK.Memory:
+                            val = "M";
+                            break;
+                        case OK.Immediate8:
+                        case OK.Immediate8to16:
+                            val = $"{i.GetImmediate(n):x2}";
+                            break;
+                        case OK.Immediate16:
+                            val = $"{i.GetImmediate(n):x4}";
+                            break;
+                    }
+                    return val;
+                })
+            );
     }
 }
--- src/PoViEmu.Core/Hardware/NC3022.cs
+++ src/PoViEmu.Core/Hardware/NC3022.cs
@@ -1,6 +1,9 @@
 using System;
 using Iced.Intel;
+using PoViEmu.Common;
 using PoViEmu.Core.Decoding;
+using R = Iced.Intel.Register;
+using OK = Iced.Intel.OpKind;
 
 // ReSharper disable InconsistentNaming
 
@@ -14,23 +17,326 @@ namespace PoViEmu.Core.Hardware
             switch (m)
             {
                 case Mnemonic.Push:
-                    if (i is { OpCount: 1, Op0Kind: OpKind.Register })
-                        switch (i.Op0Register)
-                        {
-                            case Register.CX: s.PushStack(s.CX); return;
-                            case Register.DX: s.PushStack(s.DX); return;
-                            case Register.SI: s.PushStack(s.SI); return;
-                            case Register.DI: s.PushStack(s.DI); return;
-                            default: throw new InvalidOperationException($"{i} ?");
-                        }
+                    if (i is { OpCount: 1, Op0Kind: OK.Register })
+                    {
+                        var newTop = s.Get(i.Op0Register);
+                        s.Push(newTop);
+                        return;
+                    }
                     break;
-
-                
-                
-                
-                
+                case Mnemonic.Pop:
+                    if (i is { OpCount: 1, Op0Kind: OK.Register })
+                    {
+                        s.Pop(out var oldTop);
+                        s.Set(i.Op0Register, oldTop);
+                        return;
+                    }
+                    break;
+                case Mnemonic.And:
+                    if (i is { OpCount: 2 })
+                    {
+                        if (i is { Op0Kind: OK.Register, Op1Kind: OK.Register })
+                        {
+                            // TODO
+                            return;
+                        }
+                        if (i is { Op0Kind: OK.Register, Op1Kind: OK.Memory })
+                        {
+                            // TODO
+                            return;
+                        }
+                    }
+                    break;
+                case Mnemonic.Adc:
+                    if (i is { OpCount: 2 })
+                    {
+                        if (i is { Op0Kind: OK.Register, Op1Kind: OK.Immediate16 })
+                        {
+                            // TODO
+                            return;
+                        }
+                    }
+                    break;
+                case Mnemonic.Or:
+                    if (i is { OpCount: 2 })
+                    {
+                        if (i is { Op0Kind: OK.Register, Op1Kind: OK.Register })
+                        {
+                            // TODO
+                            return;
+                        }
+                        if (i is { Op0Kind: OK.Register, Op1Kind: OK.Memory })
+                        {
+                            // TODO
+                            return;
+                        }
+                    }
+                    break;
+                case Mnemonic.Shl:
+                    if (i is { OpCount: 2 })
+                    {
+                        if (i is { Op0Kind: OK.Register, Op1Kind: OK.Register or OK.Immediate8 })
+                        {
+                            // TODO
+                            return;
+                        }
+                    }
+                    break;
+                case Mnemonic.Sar:
+                    if (i is { OpCount: 2 })
+                    {
+                        if (i is { Op0Kind: OK.Register, Op1Kind: OK.Register })
+                        {
+                            // TODO
+                            return;
+                        }
+                    }
+                    break;
+                case Mnemonic.Not:
+                    if (i is { OpCount: 1 })
+                    {
+                        if (i is { Op0Kind: OK.Register })
+                        {
+                            // TODO
+                            return;
+                        }
+                    }
+                    break;
+                case Mnemonic.Add:
+                    if (i is { OpCount: 2 })
+                    {
+                        if (i is { Op0Kind: OK.Register, Op1Kind: OK.Register or OK.Immediate16 })
+                        {
+                            var plus = (ushort)(s.Get(i.Op0Register) + s.GetValue(i, 1));
+                            s.Set(i.Op0Register, plus);
+                            return;
+                        }
+                        if (i is { Op0Kind: OK.Register, Op1Kind: OK.Memory })
+                        {
+                            // TODO
+                            return;
+                        }
+                        if (i is { Op0Kind: OK.Memory, Op1Kind: OK.Register })
+                        {
+                            // TODO
+                            return;
+                        }
+                    }
+                    break;
+                case Mnemonic.Sub:
+                    if (i is { OpCount: 2 })
+                    {
+                        if (i is { Op0Kind: OK.Register, Op1Kind: OK.Register })
+                        {
+                            var minus = (ushort)(s.Get(i.Op0Register) - s.Get(i.Op1Register));
+                            s.Set(i.Op0Register, minus);
+                            return;
+                        }
+                        if (i is { Op0Kind: OK.Register, Op1Kind: OK.Memory })
+                        {
+                            // TODO
+                            return;
+                        }
+                        if (i is { Op0Kind: OK.Memory, Op1Kind: OK.Immediate8to16 })
+                        {
+                            // TODO
+                            return;
+                        }
+                        if (i is { Op0Kind: OK.Register, Op1Kind: OK.Immediate16 or OK.Immediate8to16 })
+                        {
+                            // TODO
+                            return;
+                        }
+                    }
+                    break;
+                case Mnemonic.Retf:
+                    if (i is { OpCount: 0 })
+                    {
+                        // TODO
+                        return;
+                    }
+                    break;
+                case Mnemonic.Cwd:
+                    if (i is { OpCount: 0 })
+                    {
+                        // TODO
+                        return;
+                    }
+                    break;
+                case Mnemonic.Cbw:
+                    if (i is { OpCount: 0 })
+                    {
+                        // TODO
+                        return;
+                    }
+                    break;
+                case Mnemonic.Mul:
+                    if (i is { OpCount: 1 })
+                    {
+                        /* TODO */
+                        return;
+                    }
+                    break;
+                case Mnemonic.Idiv:
+                    if (i is { OpCount: 1 })
+                    {
+                        /* TODO */
+                        return;
+                    }
+                    break;
+                case Mnemonic.Jne:
+                    if (i is { OpCount: 1 })
+                    {
+                        /* TODO */
+                        return;
+                    }
+                    break;
+                case Mnemonic.Je:
+                    if (i is { OpCount: 1 })
+                    {
+                        /* TODO */
+                        return;
+                    }
+                    break;
+                case Mnemonic.Jl:
+                    if (i is { OpCount: 1 })
+                    {
+                        /* TODO */
+                        return;
+                    }
+                    break;
+                case Mnemonic.Jle:
+                    if (i is { OpCount: 1 })
+                    {
+                        /* TODO */
+                        return;
+                    }
+                    break;
+                case Mnemonic.Jge:
+                    if (i is { OpCount: 1 })
+                    {
+                        /* TODO */
+                        return;
+                    }
+                    break;
+                case Mnemonic.Jmp:
+                    if (i is { OpCount: 1 })
+                    {
+                        /* TODO */
+                        return;
+                    }
+                    break;
+                case Mnemonic.Dec:
+                    if (i is { OpCount: 1 })
+                    {
+                        if (i is { Op0Kind: OK.Register })
+                        {
+                            var dec = (ushort)(s.Get(i.Op0Register) - 1);
+                            s.Set(i.Op0Register, dec);
+                            return;
+                        }
+                        if (i is { Op0Kind: OK.Memory })
+                        {
+                            // TODO
+                            return;
+                        }
+                    }
+                    break;
+                case Mnemonic.Inc:
+                    if (i is { OpCount: 1 })
+                    {
+                        if (i is { Op0Kind: OK.Register })
+                        {
+                            var inc = (ushort)(s.Get(i.Op0Register) + 1);
+                            s.Set(i.Op0Register, inc);
+                            return;
+                        }
+                        if (i is { Op0Kind: OK.Memory })
+                        {
+                            // TODO
+                            return;
+                        }
+                    }
+                    break;
+                case Mnemonic.Test:
+                    if (i is { OpCount: 2 })
+                    {
+                        if (i is { Op0Kind: OK.Register, Op1Kind: OK.Register })
+                        {
+                            // TODO
+                            return;
+                        }
+                    }
+                    break;
+                case Mnemonic.Cmp:
+                    if (i is { OpCount: 2 })
+                    {
+                        if (i is { Op0Kind: OK.Register, Op1Kind: OK.Register })
+                        {
+                            // TODO (OF, SF, ZF, AF, PF, CF)
+                            var diff = s.Get(i.Op0Register) - s.Get(i.Op1Register);
+                            s.ZF = diff == 0;
+                            return;
+                        }
+                        if (i is { Op0Kind: OK.Register, Op1Kind: OK.Memory })
+                        {
+                            // TODO
+                            return;
+                        }
+                        if (i is { Op0Kind: OK.Register, Op1Kind: OK.Immediate16 or OK.Immediate8to16 })
+                        {
+                            // TODO
+                            return;
+                        }
+                        if (i is { Op0Kind: OK.Memory, Op1Kind: OK.Immediate16 or OK.Immediate8to16 })
+                        {
+                            // TODO
+                            return;
+                        }
+                    }
+                    break;
+                case Mnemonic.Xor:
+                    if (i is { OpCount: 2 })
+                    {
+                        if (i is { Op0Kind: OK.Register, Op1Kind: OK.Register })
+                        {
+                            // TODO
+                            return;
+                        }
+                        if (i is { Op0Kind: OK.Register, Op1Kind: OK.Memory })
+                        {
+                            // TODO
+                            return;
+                        }
+                    }
+                    break;
+                case Mnemonic.Mov:
+                    if (i is { OpCount: 2 })
+                    {
+                        if (i is { Op0Kind: OK.Register, Op1Kind: OK.Immediate16 or OK.Register })
+                        {
+                            s.Set(i.Op0Register, s.GetValue(i, 1));
+                            return;
+                        }
+                        if (i is { Op0Kind: OK.Register, Op1Kind: OK.Memory })
+                        {
+                            // TODO
+                            return;
+                        }
+                        if (i is { Op0Kind: OK.Memory, Op1Kind: OK.Register })
+                        {
+                            // TODO
+                            return;
+                        }
+                        if (i is { Op0Kind: OK.Memory, Op1Kind: OK.Immediate16 })
+                        {
+                            // TODO
+                            return;
+                        }
+                    }
+                    break;
             }
-            throw new ArgumentException($"{m} ({i}) ?!");
+            throw new ArgumentException($"{m} ({i}) {{{i.GetOpDebug()}}} ?!");
         }
     }
 }
