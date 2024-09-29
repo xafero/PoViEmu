cc6e14be8102dc786bdfc4a597696e1f18910313
xafero <xafero@users.noreply.github.com>
2024-09-29 18:43:44 +0200

    Separated files

---
+++ src/PoViEmu.Core/Decoding/Ops/I16Operand.cs
@@ -0,0 +1,4 @@
+namespace PoViEmu.Core.Decoding.Ops
+{
+    public record I16Operand(short Val) : BaseOperand;
+}
--- src/PoViEmu.Core/Decoding/Ops/U16Operand.cs
+++ src/PoViEmu.Core/Decoding/Ops/U16Operand.cs
@@ -1,6 +1,4 @@
 namespace PoViEmu.Core.Decoding.Ops
 {
     public record U16Operand(ushort Val) : BaseOperand;
-
-    public record I16Operand(short Val) : BaseOperand;
 }
