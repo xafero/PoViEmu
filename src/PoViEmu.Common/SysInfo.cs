348f76b27ddf8f6eb1c6f5a16f0cc18fd2bce881
xafero <xafero@users.noreply.github.com>
2024-10-07 19:52:41 +0200

    Adding more info

--- src/PoViEmu.Common/SysInfo.cs
+++ src/PoViEmu.Common/SysInfo.cs
@@ -1,5 +1,7 @@
+using System;
 using System.IO;
 using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace PoViEmu.Common
 {
@@ -17,5 +19,34 @@ namespace PoViEmu.Common
             full = Path.GetDirectoryName(full) ?? string.Empty;
             return full;
         }
+
+        public static SystemInfo GetInfo()
+        {
+            return new()
+            {
+                Framework = RuntimeInformation.FrameworkDescription,
+                OSDesc = RuntimeInformation.OSDescription,
+                OSArch = RuntimeInformation.OSArchitecture.ToString(),
+                OSVer = Environment.OSVersion.ToString(),
+                HostName = Environment.MachineName,
+                UserName = $"{Environment.UserDomainName}\\{Environment.UserName}",
+                ProcPath = Environment.ProcessPath,
+                CurrentDir = Environment.CurrentDirectory,
+                ProcId = Environment.ProcessId
+            };
+        }
+    }
+
+    public record SystemInfo
+    {
+        public string? Framework { get; init; }
+        public string? OSDesc { get; init; }
+        public string? OSArch { get; init; }
+        public string? OSVer { get; init; }
+        public string? HostName { get; init; }
+        public string? UserName { get; init; }
+        public string? ProcPath { get; init; }
+        public string? CurrentDir { get; init; }
+        public int? ProcId { get; init; }
     }
 }
