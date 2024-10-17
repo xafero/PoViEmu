eaf3bae9b373019424e8dbfd87eff6033d92604a
xafero <xafero@users.noreply.github.com>
2024-10-17 18:16:31 +0200

    Moving files

--- src/PoViEmu.Core/Hardware/FlagTool.cs
+++ src/PoViEmu.Core/Hardware/FlagTool.cs
@@ -61,14 +61,4 @@ namespace PoViEmu.Core.Hardware
             }
         }
     }
-
-    public interface IFlagged
-    {
-        bool CarryFlag { set; }
-        bool AuxCarryFlag { set; }
-        bool ParityFlag { set; }
-        bool SignFlag { set; }
-        bool ZeroFlag { set; }
-        bool OverflowFlag { set; }
-    }
 }
