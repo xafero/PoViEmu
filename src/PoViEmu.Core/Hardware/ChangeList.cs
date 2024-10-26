3f59cee2c02c12b58d269de7d70e030a3674e021
xafero <xafero@users.noreply.github.com>
2024-10-27 00:50:11 +0200

    Prepared change list for test

--- src/PoViEmu.Core/Hardware/ChangeList.cs
+++ src/PoViEmu.Core/Hardware/ChangeList.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Collections.Concurrent;
 using System.ComponentModel;
+using System.Linq;
 using PoViEmu.Common;
 
 namespace PoViEmu.Core.Hardware
 {
-    public sealed class ChangeList:IDisposable
+    public sealed class ChangeList : IDisposable
     {
         private readonly MachineState _m;
         private readonly ConcurrentDictionary<string, object> _v;
@@ -15,54 +16,43 @@ namespace PoViEmu.Core.Hardware
             _m = m;
             _v = new ConcurrentDictionary<string, object>();
             _m.PropertyChanging += OnPropertyChanging;
-            _m.PropertyChanged  += OnPropertyChanged;
+            _m.PropertyChanged += OnPropertyChanged;
         }
-        
+
         private void OnPropertyChanging(object? sender, PropertyChangingEventArgs e)
         {
             var key = e.PropertyName.TrimNull();
-            if (key==null)
+            if (key == null)
                 return;
             var state = (MachineState)sender!;
             var oldVal = state[e.PropertyName];
             _v[key] = oldVal;
         }
-        
+
         private void OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
         {
             var key = e.PropertyName.TrimNull();
-            if (key==null)
+            if (key == null)
                 return;
             var state = (MachineState)sender!;
             var newVal = state[e.PropertyName];
             _v.TryGetValue(key, out var oldVal);
-            var pair = (oldVal, newVal);
-            PropertyChanged?.Invoke(this, new PropertyEventArgs(key, pair));
-            _v[key] = pair;
+            var evt = new PropertyEventArgs(key, (oldVal, newVal));
+            PropertyChanged?.Invoke(this, evt);
+            _v[key] = evt;
         }
-        
+
         public event PropertyEventHandler? PropertyChanged;
-        
+
+        public PropertyEventArgs[] Changes
+            => _v.Select(i => i.Value)
+                .OfType<PropertyEventArgs>().ToArray();
+
         public void Dispose()
         {
-
-
-            
-            foreach (var t in _v)
-            {
-                Console.WriteLine(t.Key);
-                Console.WriteLine(t.Value+" / "+t.Value.GetType());
-            }
-            
-            
-            
-            
-            
-            
-            
             _v.Clear();
             _m.PropertyChanging -= OnPropertyChanging;
-            _m.PropertyChanged  -= OnPropertyChanged;
+            _m.PropertyChanged -= OnPropertyChanged;
         }
     }
 }
