b9054811e09c5f3ae0cdf1653d86790c8628695b
xafero <xafero@users.noreply.github.com>
2024-10-29 19:52:08 +0100

    Adding new console

---
+++ tamp/PoViEmu.CpuFan/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using static PoViEmu.Common.FileHelper;
+
+namespace PoViEmu.CpuFan
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var root = Environment.CurrentDirectory;
+            var folder = Path.GetFullPath(Path.Combine(root,
+                "..", "..", "..", "SimuHacks", "Projs", "Raw"));
+            Console.WriteLine($"Root = {folder}");
+
+            foreach (var (file, bytes) in FindLoadFiles(folder, ".com")
+                         .OrderBy(j => j.Item2.Length))
+            {
+                var name = Path.GetFileName(file);
+                Console.WriteLine($" * {name}");
+
+                // TODO
+                Console.WriteLine($"    --> {bytes.Length} bytes");
+            }
+        }
+    }
+}
