5d5f438e216537b6da21b1301f1078d2e9e54aef
xafero <xafero@users.noreply.github.com>
2024-12-07 20:44:05 +0100

    Creating html report

--- tamp/PoViEmu.Workbook/Program.cs
+++ tamp/PoViEmu.Workbook/Program.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using PoViEmu.Common;
-using PoViEmu.Core.Addins;
-using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.IO;
-using System.Linq;
-using System.Text;
-using PoViEmu.Common;
+using System.Xml.Linq;
 using PoViEmu.Core.Compat;
 using PoViEmu.Core.Hardware;
 using static PoViEmu.Common.FileHelper;
+
+// ReSharper disable CoVariantArrayConversion
 
 namespace PoViEmu.Workbook
 {
@@ -22,11 +21,45 @@ namespace PoViEmu.Workbook
                 "test", "PoViEmu.Tests", "Resources", "Compat");
             Console.WriteLine($"Root = {folder}");
 
-            var files = FindLoadFiles(folder, ".com").OrderBy(j => j.bytes.Length);
-            foreach (var (file, bytes) in files)
-            {
-                Console.WriteLine(" ? " + file + " " + bytes.Length);
+            var outDir = "output".GetOrCreateDir();
+            Console.WriteLine($"Dest = {outDir}");
 
+            var files = FindLoadFiles(folder, ".com")
+                .ToImmutableSortedDictionary(k => Path.GetFileNameWithoutExtension(k.file), v => v);
+            foreach (var (key, (name, bytes)) in files)
+            {
+                Console.WriteLine(" ? " + key + " " + name + " " + bytes.Length);
+
+                var title = $"Report ({DateTime.Now:u})";
+                var doc = HtmlHelper.CreateDoc(title);
+
+                var head = (XElement)doc.Root!.FirstNode!;
+                head.Add(new XElement("style", HtmlHelper.ToText(
+                    "body { font-family: Arial, sans-serif; margin: 20px; }",
+                    "table { width: 100%; border-collapse: collapse; margin-top: 20px; }",
+                    "th, td { border: 1px solid #ccc; padding: 10px; text-align: left; }",
+                    "th { background-color: #f4f4f4; }")));
+
+                var table = HtmlHelper.CreateTable();
+                
+                var thead = (XElement)table.FirstNode!;
+                thead.Add(new XElement("tr",
+                    HtmlHelper.Repeat("th", "Product", "Quantity", "Price", "Total"))
+                );
+                
+                var tbody = (XElement)table.LastNode!;
+                tbody.Add(new XElement("tr",
+                    HtmlHelper.Repeat("td", "item1", "item2", "item3", "item4"))
+                );
+                
+                var body = (XElement)doc.Root!.LastNode!;
+                body.Add(table);
+                
+                var xml = HtmlHelper.AsBytes(doc);
+                var xFile = Path.Combine(outDir, $"{key}.html");
+                File.WriteAllBytes(xFile, xml);
+                
+                /*
                 var c = new NC3022();
                 var m = new MachineState();
                 m.InitForCom(loadSeg: 0x0750, cxInit: 0x002C, axInit: 0xFFFF);
@@ -53,6 +86,7 @@ namespace PoViEmu.Workbook
                     }
                     if (count++ >= 150) break;
                 }
+                */
             }
         }
 
