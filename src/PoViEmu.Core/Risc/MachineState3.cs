71eba219e6a17f1858ea751bcbcc97595bc7ca29
xafero <xafero@users.noreply.github.com>
2024-12-29 04:13:22 +0100

    Adding some proto

---
+++ src/PoViEmu.Core/Risc/MachineState3.cs
@@ -0,0 +1,400 @@
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using PoViEmu.Core.Decoding.Ops.Regs;
+using PoViEmu.Core.Hardware.AckNow;
+using static PoViEmu.Core.Hardware.MachTool;
+using Fl = PoViEmu.Core.Hardware.Flagged;
+using SixLabors.ImageSharp.ColorSpaces;
+
+namespace PoViEmu.Core.Risc
+{
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public sealed class MachineState3 : INotifyPropertyChanging, INotifyPropertyChanged
+    {
+        #region General registers
+
+        private uint _r0;
+
+        public uint R0
+        {
+            get => _r0;
+            set => SetProperty(ref _r0, value);
+        }
+
+        private uint _r1;
+
+        public uint R1
+        {
+            get => _r1;
+            set => SetProperty(ref _r1, value);
+        }
+
+        private uint _r2;
+
+        public uint R2
+        {
+            get => _r2;
+            set => SetProperty(ref _r2, value);
+        }
+
+        private uint _r3;
+
+        public uint R3
+        {
+            get => _r3;
+            set => SetProperty(ref _r3, value);
+        }
+
+        private uint _r4;
+
+        public uint R4
+        {
+            get => _r4;
+            set => SetProperty(ref _r4, value);
+        }
+
+        private uint _r5;
+
+        public uint R5
+        {
+            get => _r5;
+            set => SetProperty(ref _r5, value);
+        }
+
+        private uint _r6;
+
+        public uint R6
+        {
+            get => _r6;
+            set => SetProperty(ref _r6, value);
+        }
+
+        private uint _r7;
+
+        public uint R7
+        {
+            get => _r7;
+            set => SetProperty(ref _r7, value);
+        }
+
+        private uint _r8;
+
+        public uint R8
+        {
+            get => _r8;
+            set => SetProperty(ref _r8, value);
+        }
+
+        private uint _r9;
+
+        public uint R9
+        {
+            get => _r9;
+            set => SetProperty(ref _r9, value);
+        }
+
+        private uint _r10;
+
+        public uint R10
+        {
+            get => _r10;
+            set => SetProperty(ref _r10, value);
+        }
+
+        private uint _r11;
+
+        public uint R11
+        {
+            get => _r11;
+            set => SetProperty(ref _r11, value);
+        }
+
+        private uint _r12;
+
+        public uint R12
+        {
+            get => _r12;
+            set => SetProperty(ref _r12, value);
+        }
+
+        private uint _r13;
+
+        public uint R13
+        {
+            get => _r13;
+            set => SetProperty(ref _r13, value);
+        }
+
+        private uint _r14;
+
+        public uint R14
+        {
+            get => _r14;
+            set => SetProperty(ref _r14, value);
+        }
+
+        private uint _r15;
+
+        public uint R15
+        {
+            get => _r15;
+            set => SetProperty(ref _r15, value);
+        }
+
+        #endregion
+
+        #region Banked registers
+
+        private uint _r0b;
+
+        public uint R0_b
+        {
+            get => _r0b;
+            set => SetProperty(ref _r0b, value);
+        }
+
+        private uint _r1b;
+
+        public uint R1_b
+        {
+            get => _r1b;
+            set => SetProperty(ref _r1b, value);
+        }
+
+        private uint _r2b;
+
+        public uint R2_b
+        {
+            get => _r2b;
+            set => SetProperty(ref _r2b, value);
+        }
+
+        private uint _r3b;
+
+        public uint R3_b
+        {
+            get => _r3b;
+            set => SetProperty(ref _r3b, value);
+        }
+
+        private uint _r4b;
+
+        public uint R4_b
+        {
+            get => _r4b;
+            set => SetProperty(ref _r4b, value);
+        }
+
+        private uint _r5b;
+
+        public uint R5_b
+        {
+            get => _r5b;
+            set => SetProperty(ref _r5b, value);
+        }
+
+        private uint _r6b;
+
+        public uint R6_b
+        {
+            get => _r6b;
+            set => SetProperty(ref _r6b, value);
+        }
+
+        private uint _r7b;
+
+        public uint R7_b
+        {
+            get => _r7b;
+            set => SetProperty(ref _r7b, value);
+        }
+
+        #endregion
+
+        #region Special registers
+
+        private uint _gbr;
+
+        /// <summary>
+        /// Global Base Register
+        /// </summary>
+        public uint GBR
+        {
+            get => _gbr;
+            set => SetProperty(ref _gbr, value);
+        }
+
+        private uint _mach;
+
+        /// <summary>
+        /// Multiply-Accumulate High Register
+        /// </summary>
+        public uint MACH
+        {
+            get => _mach;
+            set => SetProperty(ref _mach, value);
+        }
+
+        private uint _macl;
+
+        /// <summary>
+        /// Multiply-Accumulate Low Register
+        /// </summary>
+        public uint MACL
+        {
+            get => _macl;
+            set => SetProperty(ref _macl, value);
+        }
+
+        private uint _pr;
+
+        /// <summary>
+        /// Procedure Register
+        /// </summary>
+        public uint PR
+        {
+            get => _pr;
+            set => SetProperty(ref _pr, value);
+        }
+
+        private uint _pc;
+
+        /// <summary>
+        /// Program Counter
+        /// </summary>
+        public uint PC
+        {
+            get => _pc;
+            set => SetProperty(ref _pc, value);
+        }
+
+        private uint _sr;
+
+        /// <summary>
+        /// Status Register
+        /// </summary>
+        public uint SR
+        {
+            get => _sr;
+            set => SetProperty(ref _sr, value);
+        }
+
+        private uint _vbr;
+
+        /// <summary>
+        /// Vector Base Register
+        /// </summary>
+        public uint VBR
+        {
+            get => _vbr;
+            set => SetProperty(ref _vbr, value);
+        }
+
+        private uint _spc;
+
+        /// <summary>
+        /// Saved Program Counter
+        /// </summary>
+        public uint SPC
+        {
+            get => _spc;
+            set => SetProperty(ref _spc, value);
+        }
+
+        private uint _ssr;
+
+        /// <summary>
+        /// Saved Status Register
+        /// </summary>
+        public uint SSR
+        {
+            get => _ssr;
+            set => SetProperty(ref _ssr, value);
+        }
+
+        #endregion
+
+        #region Control flags
+
+        private Fl _f;
+
+        /// <summary>
+        /// Multiplier flag
+        /// </summary>
+        public bool M
+        {
+            get => Fl.Carry.Check(ref _f);
+            set => SetProperty(ref _f, Fl.Carry.Add(_f, value));
+        }
+
+        /// <summary>
+        /// Quotient flag
+        /// </summary>
+        public bool Q
+        {
+            get => Fl.Carry.Check(ref _f);
+            set => SetProperty(ref _f, Fl.Carry.Add(_f, value));
+        }
+
+        /// <summary>
+        /// Sign flag
+        /// </summary>
+        public bool S
+        {
+            get => Fl.Carry.Check(ref _f);
+            set => SetProperty(ref _f, Fl.Carry.Add(_f, value));
+        }
+
+        /// <summary>
+        /// Test flag
+        /// </summary>
+        public bool T
+        {
+            get => Fl.Carry.Check(ref _f);
+            set => SetProperty(ref _f, Fl.Carry.Add(_f, value));
+        }
+
+        #endregion
+
+        #region Changing events
+
+        public event PropertyChangingEventHandler? PropertyChanging;
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        [DebuggerNonUserCode]
+        [ExcludeFromCodeCoverage]
+        private void OnPropertyChanging([CallerMemberName] string? name = null)
+        {
+            var evt = new PropertyChangingEventArgs(name);
+            PropertyChanging?.Invoke(this, evt);
+        }
+
+        [DebuggerNonUserCode]
+        [ExcludeFromCodeCoverage]
+        private void OnPropertyChanged([CallerMemberName] string? name = null)
+        {
+            var evt = new PropertyChangedEventArgs(name);
+            PropertyChanged?.Invoke(this, evt);
+        }
+
+        [DebuggerNonUserCode]
+        [ExcludeFromCodeCoverage]
+        private void SetProperty<T>(ref T field, T newValue, [CallerMemberName] string? name = null,
+            IEqualityComparer<T>? comparer = null)
+        {
+            if ((comparer ?? EqualityComparer<T>.Default).Equals(field, newValue))
+                return;
+            OnPropertyChanging(name);
+            field = newValue;
+            OnPropertyChanged(name);
+        }
+
+        #endregion
+    }
+}
---
+++ src/PoViEmu.Core/Risc/SH7291.cs
@@ -0,0 +1,13 @@
+
+// ReSharper disable InconsistentNaming
+
+namespace PoViEmu.Core.Risc
+{
+    /// <summary>
+    /// The SH7291 is a custom 32bit CPU developed by CASIO based on Hitachi’s SH3
+    /// </summary>
+    public sealed class SH7291
+    {
+        public bool Halted { get; set; }
+    }
+}
