a72695ced7c87bfbd9ad8aecc36a2bacd558f63c
xafero <xafero@users.noreply.github.com>
2025-01-03 20:49:51 +0100

    Adding scripts

--- build.bat
+++ build.bat
@@ -4,7 +4,9 @@ echo  ### Build ###
 dotnet build
 
 echo  ### Clean ### 
-rd /s /q test\PoViEmu.Tests\TestResults
+rd /s /q test\PoViEmu.Tests.ABI\TestResults
+rd /s /q test\PoViEmu.Tests.CPU\TestResults
+rd /s /q test\PoViEmu.Tests.Gfx\TestResults
 
 echo  ### Test ### 
 dotnet test --collect:"XPlat Code Coverage"
