3028aafd38ed6e3578597eb79d8911abdf452d8f
xafero <xafero@users.noreply.github.com>
2025-01-04 01:16:33 +0100

    Adding project

---
+++ extra/PoViEmu.UI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace PoViEmu.UI.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ extra/PoViEmu.UI/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PoViEmu.UI.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
