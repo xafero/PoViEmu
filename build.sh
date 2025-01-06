458d469ba83379ddd0fafc713a91012712891687
xafero <xafero@users.noreply.github.com>
2025-01-06 23:23:33 +0100

    Adding test for decoding

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ echo " ### Clean ### "
 rm -R test/PoViEmu.Tests.ABI/TestResults
 rm -R test/PoViEmu.Tests.CPU/TestResults
 rm -R test/PoViEmu.Tests.Gfx/TestResults
+rm -R test/PoViEmu.Tests.ISA/TestResults
 
 echo " ### Test ### "
 dotnet test --collect:"XPlat Code Coverage"
