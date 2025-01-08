16f93137f044d3df5719d93986be351b823da2ee
xafero <xafero@users.noreply.github.com>
2025-01-08 21:20:18 +0100

    Adapted mu to be operands

--- src/PoViEmu.SH3.ISA/Ops/Mems/MemOperand.cs
+++ src/PoViEmu.SH3.ISA/Ops/Mems/MemOperand.cs
@@ -1,29 +1,33 @@
+using System;
+using PoViEmu.SH3.ISA;
 using B32 = PoViEmu.SH3.ISA.ShRegister;
 
 namespace PoViEmu.SH3.ISA.Ops.Mems
 {
-    public abstract record MemOperand<T>(B32 Seg, B32? Base, B32? Idx, short? Disp)
-        : MemOperand(Seg, Base, Idx, Disp)
+    public enum AddressingMode
+    {
+        Unknown = 0, RegisterIndirect, PostIncrement, PreDecrement, Displacement, Indexed
+    }
+
+    public abstract record MemOperand<T>(AddressingMode Mode, B32 Base, B32? Idx = null, int? Disp = null)
+        : MemOperand(Mode, Base, Idx, Disp)
     {
         public abstract T this[IMachineState m] { get; set; }
 
         public sealed override string ToString()
         {
-            var type = typeof(T).Name;
-            var oBase = HasBase ? $"{Base}+" : "";
-            var oIdx = HasIdx ? $"{Idx}+" : "";
-            var onlyD = !HasBase && !HasIdx && HasDisp;
-            var oDisp = onlyD ? $"{Disp:x4}" : HasDisp ? $"{Disp}" : "";
-            var txt = $"{type} [{Seg}:{oBase}{oIdx}{oDisp}]";
-            txt = txt.Replace("+-", "-").Replace("+]", "]");
-            return txt;
+            return Mode switch
+            {
+                AddressingMode.RegisterIndirect => $"@{Base}",
+                AddressingMode.PostIncrement => $"@{Base}+",
+                AddressingMode.PreDecrement => $"@-{Base}",
+                AddressingMode.Displacement => $"@({Disp},{Base})",
+                AddressingMode.Indexed => $"@({Idx},{Base})",
+                _ => throw new InvalidOperationException($"{Mode} ?!")
+            };
         }
-
-        public bool HasIdx => Idx != null && Idx != B32.Unknown;
-        public bool HasBase => Base != null && Base != B32.Unknown;
-        public bool HasDisp => Disp != null && Disp != 0;
     }
 
-    public abstract record MemOperand(B32 Seg, B32? Base, B32? Idx, short? Disp)
+    public abstract record MemOperand(AddressingMode Mode, B32 Base, B32? Idx = null, int? Disp = null)
         : BaseOperand;
 }
--- src/PoViEmu.SH3.ISA/Ops/Mems/Mu8Operand.cs
+++ src/PoViEmu.SH3.ISA/Ops/Mems/Mu8Operand.cs
@@ -1,14 +1,38 @@
-using B16 = PoViEmu.SH3.ISA.ShRegister;
+using B32 = PoViEmu.SH3.ISA.ShRegister;
 
 namespace PoViEmu.SH3.ISA.Ops.Mems
 {
-    public record Mu8Operand(B16 Seg, B16? Base, B16? Idx, short? Disp)
-        : MemOperand<byte>(Seg, Base, Idx, Disp)
+    public record Mu8Operand(AddressingMode Mode, B32 Base, B32? Idx, int? Disp)
+        : MemOperand<byte>(Mode, Base, Idx, Disp)
     {
         public override byte this[IMachineState m]
         {
             get => m.U8[this.OffA(m)];
             set => m.U8[this.OffA(m)] = value;
+        }
+
+        public override string ToDebug(bool v) => ToString();
+    }
+
+    public record Mu16Operand(AddressingMode Mode, B32 Base, B32? Idx, int? Disp)
+        : MemOperand<ushort>(Mode, Base, Idx, Disp)
+    {
+        public override ushort this[IMachineState m]
+        {
+            get => m.U16[this.OffA(m)];
+            set => m.U16[this.OffA(m)] = value;
+        }
+
+        public override string ToDebug(bool v) => ToString();
+    }
+
+    public record Mu32Operand(AddressingMode Mode, B32 Base, B32? Idx, int? Disp)
+        : MemOperand<uint>(Mode, Base, Idx, Disp)
+    {
+        public override uint this[IMachineState m]
+        {
+            get => m.U32[this.OffA(m)];
+            set => m.U32[this.OffA(m)] = value;
         }
 
         public override string ToDebug(bool v) => ToString();
