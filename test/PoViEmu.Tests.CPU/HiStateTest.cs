9e3c829279c1a992e40affc2df3c5ade97270438
xafero <xafero@users.noreply.github.com>
2025-01-12 07:05:24 +0100

    Fixing stack test

--- test/PoViEmu.Tests.CPU/HiStateTest.cs
+++ test/PoViEmu.Tests.CPU/HiStateTest.cs
@@ -15,49 +15,68 @@ namespace PoViEmu.Tests.CPU
         {
             var fmt = Defaults.ValFormatter;
 
-            const uint SS = 0x2050;
-            const uint CS = 0x1049;
+            const int ss = 0x2000;
+            const int cs = 0x1000;
 
             MachineState t = default;
             Action<MachineState> init = m =>
             {
-                m.R15 = 0x0008;
-                m.PC = 0x0004;
-                m[SS + 0x0000] =
+                m.R15 = ss;
+                m.PC = cs;
+
+                m[ss] =
                 [
                     0xEF, 0xCD, 0xAB, 0x89, 0x67, 0x45, 0x23, 0x01, 0xBB, 0xAA,
                     0x99, 0x88, 0x77, 0x66, 0x55, 0x44, 0x33, 0x22, 0x11, 0x00
                 ];
-                m[CS + 0x0000] =
+                m[cs] =
                 [
-                    0xe0, 0x81, // mov     #-127,r0
-                    0x2f, 0x06, // mov.l   r0,@-r15
-                    0x60, 0xf6, // mov.l   @r15+,r0
-                    0x61, 0xf6, // mov.l   @r15+,r1
-                    0xc3, 0x00, // trapa   #0
-                    0x00, 0x0b, // rts
+                    0xE0, 0x65, // mov #101,r0
+                    0xE1, 0x2E, // mov #46,r1
+                    0x00, 0x17, // mul.l r1,r0
+                    0x00, 0x1A, // sts macl,r0
+                    0x70, 0x0E, // add #14,r0
+                    0x7F, 0x08, // add     #8,r15
+                    0x2f, 0x05, // mov.w   r0,@-r15 
+                    0x60, 0xf5, // mov.w   @r15+,r0 
+                    0x61, 0xf5, // mov.w   @r15+,r1  
+                    0x00, 0x1b, // sleep  
                     0x00, 0x09 // nop
                 ];
 
                 t = m;
-                Assert.Equal("0x2301", $"{fmt.Format(t.U16[SS + 0x0006])}");
+                Assert.Equal("0x2301", $"{fmt.Format(t.U16[ss + 0x0006])}");
             };
 
             var (@out, ret, diff) = Execute([], act: init);
             var actual = diff.ToChangeLines(fmt, ignoreIP: false);
 
             TestTool.Equal([
-                /*"R0 = 0x0000 --> 0x1234",
-                "SP = 0x1000 --> 0x0FFC",
-                "SP = 0x0FFC --> 0x1000",
-                "PC = 0x0004 --> 0x0006",
-                "PC = 0x0006 --> 0x0008",
-                "PC = 0x0008 --> 0x000A"*/ // TODO
+                "R0 = 0x00000000 --> 0x00000065",
+                "PC = 0x00001000 --> 0x00001002",
+                "R1 = 0x00000000 --> 0x0000002E",
+                "PC = 0x00001002 --> 0x00001004",
+                "MACL = 0x00000000 --> 0x00001226",
+                "PC = 0x00001004 --> 0x00001006",
+                "R0 = 0x00000065 --> 0x00001226",
+                "PC = 0x00001006 --> 0x00001008",
+                "R0 = 0x00001226 --> 0x00001234",
+                "PC = 0x00001008 --> 0x0000100A",
+                "R15 = 0x00002000 --> 0x00002008",
+                "PC = 0x0000100A --> 0x0000100C",
+                "R15 = 0x00002008 --> 0x00002006",
+                "U16|00002006 = 0x2301 --> 0x1234",
+                "PC = 0x0000100C --> 0x0000100E",
+                "R15 = 0x00002006 --> 0x00002008",
+                "PC = 0x0000100E --> 0x00001010",
+                "R15 = 0x00002008 --> 0x0000200A",
+                "R1 = 0x0000002E --> 0x0000BBAA",
+                "PC = 0x00001010 --> 0x00001012"
             ], actual);
 
             Assert.Equal("", $"{@out}");
             Assert.Equal("", $"{ret}");
-            // TODO Assert.Equal("0x1234", $"{fmt.Format(t.U16[SS + 0x0006])}");
+            Assert.Equal("0x1234", $"{fmt.Format(t.U16[ss + 0x0006])}");
         }
     }
 }
