b614129f5fe1e20d474d8835cfa2eca6d1f764f2
xafero <xafero@users.noreply.github.com>
2025-01-14 23:28:43 +0100

    Setting methods to private

--- src/PoViEmu.SH3.ISA/Core/ArgTool.cs
+++ src/PoViEmu.SH3.ISA/Core/ArgTool.cs
@@ -18,43 +18,43 @@ namespace PoViEmu.SH3.ISA.Core
         public static BaseOperand W(byte a, ShRegister b)
             => W(InstTool.GetReg(a), b);
 
-        public static BaseOperand W(ShRegister a, ShRegister b)
+        private static BaseOperand W(ShRegister a, ShRegister b)
             => new Mu16Operand(AM.Indexed, a, b, null);
 
         public static BaseOperand L(byte a, ShRegister b)
             => L(InstTool.GetReg(a), b);
 
-        public static BaseOperand L(ShRegister a, ShRegister b)
+        private static BaseOperand L(ShRegister a, ShRegister b)
             => new Mu32Operand(AM.Indexed, a, b, null);
 
         public static BaseOperand B((byte high, byte low) t) => B(t.high, t.low);
 
-        public static BaseOperand B(byte regNo, int disp) => B(InstTool.GetReg(regNo), disp);
+        private static BaseOperand B(byte regNo, int disp) => B(InstTool.GetReg(regNo), disp);
 
         public static BaseOperand B(ShRegister reg, int disp)
             => new Mu8Operand(AM.Displacement, reg, null, disp);
 
         public static BaseOperand W((byte high, byte low) t, int factor = 2) => W(t.high, t.low * factor);
 
-        public static BaseOperand W(byte regNo, int disp) => W(InstTool.GetReg(regNo), disp);
+        private static BaseOperand W(byte regNo, int disp) => W(InstTool.GetReg(regNo), disp);
 
         public static BaseOperand W(ShRegister reg, byte disp, int factor = 2) => W(reg, disp * factor);
 
-        public static BaseOperand W(ShRegister reg, int disp)
+        private static BaseOperand W(ShRegister reg, int disp)
             => new Mu16Operand(AM.Displacement, reg, null, disp);
 
         public static BaseOperand L((byte high, byte low) t, int factor = 4) => L(t.high, t.low * factor);
 
-        public static BaseOperand L(byte regNo, int disp) => L(InstTool.GetReg(regNo), disp);
+        private static BaseOperand L(byte regNo, int disp) => L(InstTool.GetReg(regNo), disp);
 
         public static BaseOperand L(ShRegister reg, byte disp, int factor = 4) => L(reg, disp * factor);
 
-        public static BaseOperand L(ShRegister reg, int disp)
+        private static BaseOperand L(ShRegister reg, int disp)
             => new Mu32Operand(AM.Displacement, reg, null, disp);
 
         public static BaseOperand I(byte imm) => new I8Operand((sbyte)imm);
         public static BaseOperand U(byte imm) => new U8Operand(imm);
-        public static BaseOperand D(uint imm) => new NearOperand((int)imm);
+        private static BaseOperand D(uint imm) => new NearOperand((int)imm);
         public static BaseOperand R(byte regNo) => new Reg32Operand(InstTool.GetReg(regNo));
 
         public static BaseOperand RrB(byte regNo)
