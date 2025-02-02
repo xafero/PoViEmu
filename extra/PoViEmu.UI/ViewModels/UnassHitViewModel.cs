2ad35db93ccfead16194ad146faec85c885a1942
xafero <xafero@users.noreply.github.com>
2025-02-02 22:55:01 +0100

    Working on it

--- extra/PoViEmu.UI/ViewModels/UnassHitViewModel.cs
+++ extra/PoViEmu.UI/ViewModels/UnassHitViewModel.cs
@@ -1,29 +1,50 @@
 using PoViEmu.Base;
 using PoViEmu.UI.Models;
 using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.ComponentModel;
+using System.IO;
+using System.Xml.Linq;
+using PoViEmu.Base;
+using PoViEmu.Base.CPU;
+using PoViEmu.Base.ISA;
+using PoViEmu.SH3.ISA.Decoding;
+using DefI = PoViEmu.I186.CPU.Impl.Defaults;
+using DefS = PoViEmu.SH3.CPU.Impl.Defaults;
+using static PoViEmu.Base.FileHelper;
 using MachineStateSH3 = PoViEmu.SH3.CPU.MachineState;
 
 namespace PoViEmu.UI.ViewModels
 {
     public class UnassHitViewModel : UnassViewModel
     {
-        public void Read(uint offset, byte[] bytes, int lineSize = 16)
+        public void Read(uint offset, byte[] bytes, int count = 25)
         {
             Lines.Clear();
-            foreach (var oneArray in bytes.SplitEvery(lineSize))
+
+            var cpuFs = DefS.CpuFactory;
+            var m = new MachineStateSH3();
+            var cpuRs = cpuFs.CreateReader(m);
+
+            var i = 0;
+            while (i <= count)
             {
-                var txt = oneArray.DecodeChars();
-                var hex = oneArray.ToHex(false, true);
+                var item = cpuRs.NextInstruction();
+                var txt = item.ToString();
+                var hex = item.Bytes;
                 var off = $"{offset:X8}";
                 Lines.Add(new BytesLine(off, hex, txt));
-                offset = (ushort)(offset + oneArray.Length);
+                offset = (ushort)(offset + (hex.Length / 2));
+                i++;
             }
         }
 
         public void Read(MachineStateSH3 state)
         {
-            var off = state.R15;
-            var bytes = state.ReadMemory(off, 512);
+            var off = state.PC;
+            var bytes = state.ReadMemory(off, 128);
             Read(off, bytes.ToArray());
         }
     }
--- extra/PoViEmu.UI/ViewModels/UnassIntViewModel.cs
+++ extra/PoViEmu.UI/ViewModels/UnassIntViewModel.cs
@@ -1,6 +1,18 @@
 using PoViEmu.Base;
 using PoViEmu.UI.Models;
 using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.ComponentModel;
+using System.IO;
+using System.Xml.Linq;
+using PoViEmu.Base;
+using PoViEmu.Base.CPU;
+using PoViEmu.Base.ISA;
+using DefI = PoViEmu.I186.CPU.Impl.Defaults;
+using DefS = PoViEmu.SH3.CPU.Impl.Defaults;
+using static PoViEmu.Base.FileHelper;
 using MachineStateI86 = PoViEmu.I186.CPU.MachineState;
 
 namespace PoViEmu.UI.ViewModels
@@ -10,6 +22,11 @@ namespace PoViEmu.UI.ViewModels
         public void Read(ushort segment, ushort offset, byte[] bytes, int lineSize = 16)
         {
             Lines.Clear();
+            
+            var cpuFi = DefI.CpuFactory;
+            var m = new MachineStateI86();
+            var cpuRi = cpuFi.CreateReader(m);
+            
             foreach (var oneArray in bytes.SplitEvery(lineSize))
             {
                 var txt = oneArray.DecodeChars();
@@ -24,7 +41,7 @@ namespace PoViEmu.UI.ViewModels
         {
             var seg = state.DS;
             var off = state.SI;
-            var bytes = state.ReadMemory(seg, off, 512);
+            var bytes = state.ReadMemory(seg, off, 128);
             Read(seg, off, bytes.ToArray());
         }
     }
