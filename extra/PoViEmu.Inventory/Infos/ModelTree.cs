16a6f97a4b0435793fa63f50a8c19604bc73c180
xafero <xafero@users.noreply.github.com>
2025-02-10 23:59:14 +0100

    Adding mod pull

--- extra/PoViEmu.Inventory/Infos/ModelTree.cs
+++ extra/PoViEmu.Inventory/Infos/ModelTree.cs
@@ -6,10 +6,11 @@ namespace PoViEmu.Inventory.Infos
     public class ModelTree
     {
         public DateTime Changed { get; set; }
-        public string Name { get; set; }
-        public Clock Clock { get; set; }
-        public int CSysRam { get; set; }
-        public Display Display { get; set; }
+        public ModelKind Kind { get; set; } 
+        public string? Name { get; set; }
+        public Clock? Clock { get; set; }
+        public int? CSysRam { get; set; }
+        public Display? Display { get; set; }
         public Dictionary<char, ChipGroup> Groups { get; set; } = new();
     }
 }
--- tools/discover/ModPull.cs
+++ tools/discover/ModPull.cs
@@ -8,6 +8,8 @@ using PoViEmu.Compression;
 using PoViEmu.Inventory.Infos;
 using PoViEmu.Inventory.Utils;
 
+// ReSharper disable InconsistentNaming
+
 namespace Discover
 {
     internal static class ModPull
@@ -33,14 +35,19 @@ namespace Discover
         internal static void Run(Options opt)
         {
             var folder = Path.GetFullPath(opt.InputDir);
+            const string cpj = ".cpj";
+            const string dlp = ".dlp";
 
-            var files = folder.FindFiles(".cpj");
+            var files = folder.FindFiles(cpj, dlp);
             var outDir = "out".GetOrCreateDir();
 
             foreach (var file in files)
             {
                 var name = Path.GetFileNameWithoutExtension(file);
-                Console.WriteLine($" * {name}");
+                name = name.Replace("PV3S1600", "PVS1600");
+                var ext = Path.GetExtension(file).ToLowerInvariant();
+                var kind = ext switch { ".cpj" => ModelKind.X86, ".dlp" => ModelKind.SH3, _ => default };
+                Console.WriteLine($" * {kind} => {name}");
 
                 var info = new FileInfo(file);
                 var changed = info.LastWriteTime;
@@ -48,86 +55,121 @@ namespace Discover
                 var oFile = Path.Combine(outDir, $"{suffix}_{name}.json");
                 Console.WriteLine($"    - {oFile}");
 
-                var ini = IniTool.LoadIni(file);
-                var dict = new ModelTree { Changed = changed, Name = name };
-
-                var general = ini["GENERAL"];
-                var currentDir = general["CurrentDir"].ToString();
-                var sourceDir = general["SourceDir"].ToString();
-                Console.WriteLine($"       # {currentDir}, {sourceDir}");
-
-                var cpuClock = int.Parse(general["CLOCK"]);
-                var lcdClock = int.Parse(general["LCDC_FREQ"]);
-                dict.Clock = new Clock(cpuClock, lcdClock);
-
-                var cSysRam = int.Parse(general["C_SYSRAM"]);
-                dict.CSysRam = cSysRam;
-
-                var tablet = ini["TABLET_DISP"];
-                var width = int.Parse(tablet["DispWidth"]);
-                var height = int.Parse(tablet["DispHeight"]);
-                dict.Display = new Display(width, height);
-
-                var inv = StringComparison.InvariantCultureIgnoreCase;
-                var chips = ini.Where(s => s.Name.StartsWith("CsGroup", inv)
-                                           || s.Name.Equals("_Inside", inv)).ToArray();
-                var chipsNames = string.Join(", ", chips.Select(s => s.Name));
-                Console.WriteLine($"       # {chipsNames}");
-
-                foreach (var chip in chips)
+                var dict = new ModelTree { Changed = changed, Name = name, Kind = kind };
+                switch (kind)
                 {
-                    var cg = chip.Name.Replace("CSGROUP", string.Empty).Replace("_INSIDE", "I").Single();
-                    var cGroup = new ChipGroup();
-
-                    var cPos = Read(chip).GroupBy(i => i.c).ToArray();
-                    foreach (var gPos in cPos)
-                    {
-                        var cDict = new Dictionary<string, string>();
-                        foreach (var tuple in gPos)
-                            cDict.Add(tuple.k, tuple.v);
-
-                        var cItm = new Chip();
-                        if (cDict.TryGetValue("NAME", out var xName))
-                            cItm.Caption = xName;
-                        if (cDict.TryGetValue("BUS", out var xBus))
-                            cItm.ConnectionBit = byte.Parse(xBus);
-                        if (cDict.TryGetValue("CODE", out var xCode))
-                            cItm.ProgChipName = xCode;
-                        if (cDict.TryGetValue("OFFSET", out var xOffset))
-                        {
-                            var xOffsetInt = Convert.ToInt32(xOffset, 16);
-                            cItm.LoadOffset = $"{xOffsetInt:X8}";
-                        }
-                        if (cDict.TryGetValue("PROGAREA", out var xProgArea))
-                        {
-                            var xProgAreaInt = Convert.ToInt32(xProgArea, 16);
-                            cItm.ProgAreaSize = $"{xProgAreaInt:X8}";
-                        }
-                        if (cDict.TryGetValue("KIND", out var xKind))
-                            cItm.ChipKind = xKind == "0" ? ChipKind.RAM :
-                                xKind == "1" ? ChipKind.ROM :
-                                xKind == "2" ? ChipKind.NorFlash :
-                                xKind == "5" ? ChipKind.MemoryIO :
-                                default;
-                        if (cDict.TryGetValue("FILE", out var xFile))
-                        {
-                            var xFileReal = PathHelper.TryGetDep(file, xFile)!;
-                            var xBytes = File.ReadAllBytes(xFileReal);
-                            var xHash = HashHelper.GetSha(xBytes);
-                            var xFName = Path.GetFileName(xFileReal);
-                            var xZip = CompressAlgo.Brotli.Compress(xBytes);
-                            cItm.File = new FileRef
-                            {
-                                Name = xFName, Size = xBytes.Length, Hash = xHash, Brotli = xZip.B
-                            };
-                            cItm.ChipSize = $"{xBytes.Length:X8}";
-                        }
-                        cGroup.Chips[gPos.Key] = cItm;
-                    }
-                    dict.Groups[cg] = cGroup;
+                    case ModelKind.X86: RunX86(file, dict); break;
+                    case ModelKind.SH3: RunSH3(file, dict); break;
                 }
                 JsonHelper.SaveToFile(dict, oFile);
             }
+        }
+
+        private static void RunX86(string file, ModelTree dict)
+        {
+            var ini = IniTool.LoadIni(file);
+            var dir = Path.GetDirectoryName(file)!;
+
+            var general = ini["GENERAL"];
+            var currentDir = PathHelper.Merge(dir, general["CurrentDir"]);
+            var sourceDir = PathHelper.Merge(dir, general["SourceDir"]);
+            if (sourceDir!.Contains("SAMPLE1"))
+                sourceDir = sourceDir.Replace("SAMPLE1", "Sample1");
+            else if (sourceDir!.Contains("SAMPLE"))
+                sourceDir = sourceDir.Replace("SAMPLE", "Sample");
+            Console.WriteLine($"       # {currentDir}, {sourceDir}");
+
+            var cpuClock = int.Parse(general["CLOCK"]);
+            var lcdClock = int.Parse(general["LCDC_FREQ"]);
+            dict.Clock = new Clock(cpuClock, lcdClock);
+
+            var cSysRam = int.Parse(general["C_SYSRAM"]);
+            dict.CSysRam = cSysRam;
+
+            var tablet = ini["TABLET_DISP"];
+            var width = int.Parse(tablet["DispWidth"]);
+            var height = int.Parse(tablet["DispHeight"]);
+            dict.Display = new Display(width, height);
+
+            var inv = StringComparison.InvariantCultureIgnoreCase;
+            var chips = ini.Where(s => s.Name.StartsWith("CsGroup", inv)
+                                       || s.Name.Equals("_Inside", inv)).ToArray();
+            var chipsNames = string.Join(", ", chips.Select(s => s.Name));
+            // Console.WriteLine($"       # {chipsNames}");
+
+            foreach (var chip in chips)
+            {
+                var cg = chip.Name.Replace("CSGROUP", string.Empty).Replace("_INSIDE", "I").Single();
+                var cGroup = new ChipGroup();
+
+                var cPos = Read(chip).GroupBy(i => i.c).ToArray();
+                foreach (var gPos in cPos)
+                {
+                    var cDict = new Dictionary<string, string>();
+                    foreach (var tuple in gPos)
+                        cDict.Add(tuple.k, tuple.v);
+
+                    var cItm = new Chip();
+                    if (cDict.TryGetValue("NAME", out var xName))
+                        cItm.Caption = xName;
+                    if (cDict.TryGetValue("BUS", out var xBus))
+                        cItm.ConnectionBit = byte.Parse(xBus);
+                    if (cDict.TryGetValue("CODE", out var xCode))
+                        cItm.ProgChipName = xCode;
+                    if (cDict.TryGetValue("OFFSET", out var xOffset))
+                    {
+                        var xOffsetInt = Convert.ToInt32(xOffset, 16);
+                        cItm.LoadOffset = $"{xOffsetInt:X8}";
+                    }
+                    if (cDict.TryGetValue("PROGAREA", out var xProgArea))
+                    {
+                        var xProgAreaInt = Convert.ToInt32(xProgArea, 16);
+                        cItm.ProgAreaSize = $"{xProgAreaInt:X8}";
+                    }
+                    if (cDict.TryGetValue("KIND", out var xKind))
+                        cItm.ChipKind = xKind == "0" ? ChipKind.RAM :
+                            xKind == "1" ? ChipKind.ROM :
+                            xKind == "2" ? ChipKind.NorFlash :
+                            xKind == "5" ? ChipKind.MemoryIO :
+                            default;
+                    if (cDict.TryGetValue("FILE", out var xFile))
+                    {
+                        var xFileReal = PathHelper.TryGetDep(file, xFile)!;
+                        var xBytes = File.ReadAllBytes(xFileReal);
+                        var xHash = HashHelper.GetSha(xBytes);
+                        var xFName = Path.GetFileName(xFileReal);
+                        var xZip = CompressAlgo.Brotli.Compress(xBytes);
+                        cItm.File = new FileRef
+                        {
+                            Name = xFName, Size = xBytes.Length, Hash = xHash, Brotli = xZip.B
+                        };
+                        cItm.ChipSize = $"{xBytes.Length:X8}";
+                    }
+                    cGroup.Chips[gPos.Key] = cItm;
+                }
+                dict.Groups[cg] = cGroup;
+            }
+        }
+
+        private static void RunSH3(string file, ModelTree dict)
+        {
+            var ini = IniTool.LoadIni(file);
+            var dir = Path.GetDirectoryName(file)!;
+
+            var general = ini["DLSimProject"];
+            var sourcePath = PathHelper.Combine(dir, general["SourcePath"]);
+            var memoryPath = PathHelper.Combine(dir, general["MemoryPath"]);
+            if (sourcePath == null) return;
+            Console.WriteLine($"       # {sourcePath}, {memoryPath}");
+
+            var prog1 = ini["Program1"];
+            var prog1F = PathHelper.Combine(dir, prog1["Program"]);
+            var prog1A = prog1["LoadAddress"].ToString();
+            Console.WriteLine("   ??? " + prog1F + " " + prog1A);
+
+            var modelRef = PathHelper.Combine(dir, general["Model"]);
+
+            // TODO
         }
     }
 }
