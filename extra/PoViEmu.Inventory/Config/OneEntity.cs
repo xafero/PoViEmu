6145a702105e8e78f603467162ed3f4414f7a759
xafero <xafero@users.noreply.github.com>
2025-02-26 00:14:36 +0100

    Working on it

--- extra/PoViEmu.Inventory/Config/OneEntity.cs
+++ extra/PoViEmu.Inventory/Config/OneEntity.cs
@@ -5,5 +5,9 @@ namespace PoViEmu.Inventory.Config
     public class OneEntity
     {
         public required Guid Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Notes { get; set; }
     }
 }
