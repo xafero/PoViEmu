69030c3bbed7c799fb6a395fc26df6544928d0a2
xafero <xafero@users.noreply.github.com>
2025-03-04 23:41:51 +0100

    Creating debug exe

---
+++ extra/PoViEmu.UI.Debugger/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Avalonia;
+
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable ClassNeverInstantiated.Global
+
+namespace PoViEmu.UI.Desktop
+{
+    internal sealed class Program
+    {
+        [STAThread]
+        public static void Main(string[] args) 
+            => BuildAvaloniaApp()   
+                .StartWithClassicDesktopLifetime(args);
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .WithInterFont()
+                .LogToTrace();
+    }
+}
