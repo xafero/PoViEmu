5c2b9ebe40094e6f1196d5eab5956d23d2cf3d68
xafero <xafero@users.noreply.github.com>
2025-03-07 19:06:06 +0100

    Moving files

---
+++ extra/PoViEmu.UI/Core/MainWindow.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace PoViEmu.UI.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Control_OnLoaded(object? sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
