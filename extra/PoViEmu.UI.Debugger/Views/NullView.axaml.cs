0278a13e08da7b76e1505d39a8f17b8b516d3c56
xafero <xafero@users.noreply.github.com>
2025-03-23 15:39:23 +0100

    Update NullView.axaml.cs

--- extra/PoViEmu.UI.Debugger/Views/NullView.axaml.cs
+++ extra/PoViEmu.UI.Debugger/Views/NullView.axaml.cs
@@ -2,11 +2,24 @@ using Avalonia.Controls;
 using Avalonia.Interactivity;
 using PoViEmu.UI.Core;
 using PoViEmu.UI.Extensions;
+using PoViEmu.UI.ViewModels;
+using Avalonia.Controls;
+using PoViEmu.UI.Extensions;
+using System;
+using Avalonia;
+using PoViEmu.UI.Core;
+using PoViEmu.UI.Dbg.Core;
+using DebugModel = PoViEmu.UI.Dbg.ViewModels.MainViewModel;
+using DebugView = PoViEmu.UI.Dbg.Views.MainView;
+using DebugWindow = PoViEmu.UI.Dbg.Core.MainWindow;
+using PoViEmu.UI.Dbg.ViewModels;
 
 namespace PoViEmu.UI.Dbg.Views
 {
     public partial class NullView : UserControl
     {
+        private DebugModel _modelRef;
+
         public NullView()
         {
             InitializeComponent();
@@ -14,12 +27,20 @@ namespace PoViEmu.UI.Dbg.Views
 
         private void OnLoaded(object? sender, RoutedEventArgs e)
         {
+            if (Parent?.Parent is DebugView { DataContext: DebugModel ctx })
+                _modelRef = ctx;
             ExtPoints.Instance.ViewChanged += OnViewChanged;
         }
 
         private void OnViewChanged(object? sender, GenArgs<IViewModelBase> e)
         {
-            // TODO
+            var model = e.Value;
+            if (model is RunInstViewModel)
+            {
+                _modelRef.CurrentView = null;
+                return;
+            }
+            _modelRef.CurrentView = Setup.Null;
         }
     }
 }
