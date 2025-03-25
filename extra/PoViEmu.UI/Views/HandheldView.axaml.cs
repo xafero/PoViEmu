0137f38126173a13df0e99302ed3513cd48f4375
xafero <xafero@users.noreply.github.com>
2025-03-25 22:39:38 +0100

    Adding view

---
+++ extra/PoViEmu.UI/Views/HandheldView.axaml.cs
@@ -0,0 +1,22 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using PoViEmu.UI.ViewModels;
+
+namespace PoViEmu.UI.Views
+{
+    public partial class HandheldView : UserControl
+    {
+        public HandheldView()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoaded(object? sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
