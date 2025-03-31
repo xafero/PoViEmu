a3ff282a85b463e5855a971db34420bda59d3f23
xafero <xafero@users.noreply.github.com>
2025-03-31 23:08:13 +0200

    Adding new convertor

---
+++ extra/PoViEmu.UI/Conv/TernaryConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+using PoViEmu.Base;
+
+namespace PoViEmu.UI.Conv
+{
+    public sealed class TernaryConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            var args = ValueHelper.AsStringArray(parameter);
+            var dest = targetType.FullName;
+            if (value is bool bv)
+            {
+                if (dest == "System.Double")
+                {
+                    var doubles = ValueHelper.AsDoubleArray(args);
+                    if (doubles is [{ } trueV, { } falseV])
+                    {
+                        return bv ? trueV : falseV;
+                    }
+                }
+            }
+            return AvaloniaProperty.UnsetValue;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return AvaloniaProperty.UnsetValue;
+        }
+    }
+}
---
+++ src/PoViEmu.Base/ValueHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace PoViEmu.Base
+{
+    public static class ValueHelper
+    {
+        public static readonly CultureInfo Ignore = CultureInfo.InvariantCulture;
+
+        public static double?[]? AsDoubleArray(IEnumerable<string>? args)
+        {
+            return args?.Select(txt => double.TryParse((string?)txt, Ignore, out var value)
+                ? value
+                : default(double?)).ToArray();
+        }
+
+        public static string[]? AsStringArray(object? raw, string sep = ";")
+        {
+            return raw switch
+            {
+                IEnumerable<string> es => es.ToArray(),
+                string txt => txt.Split(sep),
+                _ => null
+            };
+        }
+    }
+}
