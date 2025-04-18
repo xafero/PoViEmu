c52d2165667b32e9c2792975ba6b05722631b4e3
xafero <xafero@users.noreply.github.com>
2025-04-18 23:47:14 +0200

    Adding code

--- extra/PoViEmu.Hyper/NcMachine.cs
+++ extra/PoViEmu.Hyper/NcMachine.cs
@@ -1,5 +1,17 @@
 ﻿using System;
 using PoViEmu.I186.CPU;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.ComponentModel;
+using System.IO;
+using System.Xml.Linq;
+using PoViEmu.Base;
+using PoViEmu.Base.CPU;
+using PoViEmu.Base.ISA;
+using DefI = PoViEmu.I186.CPU.Impl.Defaults;
+using DefS = PoViEmu.SH3.CPU.Impl.Defaults;
+using static PoViEmu.Base.FileHelper;
 
 namespace PoViEmu.Hyper
 {
@@ -18,6 +30,24 @@ namespace PoViEmu.Hyper
 
             object ni = null;
             _cpu.Execute((I186.ISA.Decoding.XInstruction)ni, _state);
+        }
+
+        public void DoIt(byte[] bytes)
+        {
+            ICpu cpu;
+            INotifyPropertyChanged m;
+            Func<string, object> fetchProp;
+            Action<IInstruction> execThis;
+            Func<IInstruction> readNext;
+
+            var cpuFi = DefI.CpuFactory;
+            var cpuI = cpuFi.CreateCpu(bytes, out var m1);
+            var cpuRi = cpuFi.CreateReader(m1);
+            fetchProp = nn => m1[nn];
+            execThis = ni => cpuI.Execute((I186.ISA.Decoding.XInstruction)ni, m1);
+            readNext = () => cpuRi.NextInstruction();
+            cpu = cpuI;
+            m = m1;
         }
     }
 }
--- extra/PoViEmu.Hyper/ShMachine.cs
+++ extra/PoViEmu.Hyper/ShMachine.cs
@@ -1,5 +1,17 @@
 using System;
 using PoViEmu.SH3.CPU;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.ComponentModel;
+using System.IO;
+using System.Xml.Linq;
+using PoViEmu.Base;
+using PoViEmu.Base.CPU;
+using PoViEmu.Base.ISA;
+using DefI = PoViEmu.I186.CPU.Impl.Defaults;
+using DefS = PoViEmu.SH3.CPU.Impl.Defaults;
+using static PoViEmu.Base.FileHelper;
 
 namespace PoViEmu.Hyper
 {
@@ -18,6 +30,24 @@ namespace PoViEmu.Hyper
 
             object ni = null;
             _cpu.Execute((SH3.ISA.Decoding.XInstruction)ni, _state);
+        }
+
+        public void DoIt(byte[] bytes)
+        {
+            ICpu cpu;
+            INotifyPropertyChanged m;
+            Func<string, object> fetchProp;
+            Action<IInstruction> execThis;
+            Func<IInstruction> readNext;
+
+            var cpuFs = DefS.CpuFactory;
+            var cpuS = cpuFs.CreateCpu(bytes, out var m2);
+            var cpuRs = cpuFs.CreateReader(m2);
+            fetchProp = nn => m2[nn];
+            execThis = ni => cpuS.Execute((SH3.ISA.Decoding.XInstruction)ni, m2);
+            readNext = () => cpuRs.NextInstruction();
+            cpu = cpuS;
+            m = m2;
         }
     }
 }
