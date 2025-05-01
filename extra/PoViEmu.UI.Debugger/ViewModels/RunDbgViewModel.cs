97b8483e6f63b2b7037c8a8251386894d150d00f
xafero <xafero@users.noreply.github.com>
2025-05-01 16:36:39 +0200

    Splitting state

--- extra/PoViEmu.UI.Debugger/ViewModels/RunDbgViewModel.cs
+++ extra/PoViEmu.UI.Debugger/ViewModels/RunDbgViewModel.cs
@@ -4,6 +4,8 @@ using PoViEmu.UI.Routes;
 using PoViEmu.UI.Tools;
 using PoViEmu.UI.ViewModels;
 using CommunityToolkit.Mvvm.ComponentModel;
+using M186 = PoViEmu.I186.CPU.MachineState;
+using MSh3 = PoViEmu.SH3.CPU.MachineState;
 
 namespace PoViEmu.UI.Dbg.ViewModels
 {
@@ -14,5 +16,23 @@ namespace PoViEmu.UI.Dbg.ViewModels
         [ObservableProperty] private IVMachine? _currentMach;
 
         [ObservableProperty] private RunUtil.RunObj? _currentInfo;
+
+        partial void OnCurrentMachChanged(IVMachine? value)
+        {
+            switch (value?.State)
+            {
+                case M186 m186:
+                    StateN = m186;
+                    StateH = null;
+                    break;
+                case MSh3 mSh3:
+                    StateN = null;
+                    StateH = mSh3;
+                    break;
+            }
+        }
+
+        [ObservableProperty] private M186? _stateN;
+        [ObservableProperty] private MSh3? _stateH;
     }
 }
