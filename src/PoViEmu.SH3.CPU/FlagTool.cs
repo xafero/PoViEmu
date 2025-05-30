6b003380e2a84642362b8c8830b76ce8010f1ec6
xafero <xafero@users.noreply.github.com>
2025-05-30 20:05:12 +0200

    Fixing flag setting

--- src/PoViEmu.SH3.CPU/FlagTool.cs
+++ src/PoViEmu.SH3.CPU/FlagTool.cs
@@ -1,15 +1,22 @@
-using PoViEmu.SH3.ISA;
+using Fl = PoViEmu.SH3.ISA.Flagged;
 
 namespace PoViEmu.SH3.CPU
 {
     public static class FlagTool
     {
-        public static void SetFlags(this MachineState m, Flagged value)
+        public static void SetFlags(this MachineState m, Fl value)
         {
-            m.M = value.HasFlag(Flagged.M_bit);
-            m.Q = value.HasFlag(Flagged.Q_bit);
-            m.S = value.HasFlag(Flagged.S_bit);
-            m.T = value.HasFlag(Flagged.T_bit);
+            m.M = value.HasFlag(Fl.M_bit);
+            m.Q = value.HasFlag(Fl.Q_bit);
+            m.S = value.HasFlag(Fl.S_bit);
+            m.T = value.HasFlag(Fl.T_bit);
+            m.I0 = value.HasFlag(Fl.I0);
+            m.I1 = value.HasFlag(Fl.I1);
+            m.I2 = value.HasFlag(Fl.I2);
+            m.I3 = value.HasFlag(Fl.I3);
+            m.BL = value.HasFlag(Fl.Block_bit);
+            m.RB = value.HasFlag(Fl.Bank_bit);
+            m.MD = value.HasFlag(Fl.Mode_bit);
         }
     }
 }
